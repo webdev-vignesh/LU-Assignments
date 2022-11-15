cc5fce88c92f0db0e64b22271ccaae2cec197583
vignesh murugan <vicky.web3@gmail.com>
2022-11-15 09:37:29 +0530

    menu driven calculator function without parameters

---
+++ assignment 6 - Pattern-Functions/6_3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+void add() {
+    int a,b;
+    cout<<"Enter value for a and b\n";
+    cin>>a>>b;
+    int c = a+b;
+    cout<<"Addition of a and b is: "<<c<<"\n";
+}
+
+void sub() {
+    int a,b;
+    cout<<"Enter value for a and b\n";
+    cin>>a>>b;
+    int c = a-b;
+    cout<<"Subtraction of a and b is: "<<c<<"\n";
+}
+
+void mul() {
+    int a,b;
+    cout<<"Enter value for a and b\n";
+    cin>>a>>b;
+    int c = a*b;
+    cout<<"Multiplication of a and b is: "<<c<<"\n";
+}
+
+void div() {
+    int a,b;
+    cout<<"Enter value for a and b\n";
+    cin>>a>>b;
+    int c = a/b;
+    cout<<"Division of a and b is: "<<c<<"\n";
+}
+
+int main() {
+    char q;
+    do
+    {
+        int choice;
+        cout<<"Welcome to Vignesh's Calculator\n"
+        <<"What arithmetic operation would you like to perform:\n"
+        <<"Enter 1 for Addition\nEnter 2 for Subtraction\n"
+        <<"Enter 3 for Multiplication\nEnter 4 for Division\n";
+        cin>>choice;
+        if(choice == 1)
+        {
+            add();
+        }
+        else if(choice == 2)
+        {
+            sub();
+        }
+        else if(choice == 3)
+        {
+            mul();
+        }
+        else if(choice == 4)
+        {
+            div();
+        }
+        else
+        {
+            cout<<"Please select correct option";
+        }
+        cout<<"Would you like to continue?\nEnter y or n\n";
+        cin>>q;
+    }while(q == 'y');
+    cout<<"Thanks for using my calculator!!";
+    
+    return 0;
+}
