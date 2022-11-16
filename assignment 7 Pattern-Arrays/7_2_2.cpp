14034a26d2c13d3f3f7022aee046d0814214ac3a
vignesh murugan <vicky.web3@gmail.com>
2022-11-16 19:25:36 +0530

    sum of array-print sum in total function

--- assignment 7 Pattern-Arrays/7_2_2.cpp
+++ assignment 7 Pattern-Arrays/7_2_2.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 using namespace std;
 
-void total()
+void total() //function to add array of elements
 {
-    int a[5];
+    int a[5]; //declaring array in external function
     cout<<"Enter the value of each elements:\n";
-    for(int i=0; i<5; i++)
+    for(int i=0; i<5; i++) //taking user input 
     {
         cin>>a[i];
     }
     int sum=0;
-    for(int i=0; i<5; i++)
+    for(int i=0; i<5; i++) //adding values in loop
     {
         sum = sum + a[i];
     }
-    cout<<"Sum of all elements: "<<sum;
+    cout<<"Sum of all elements: "<<sum; //printing sum of array elements
 }
 
 int main() {
-    total();
+    total(); //calling external total function
 
     return 0;
 }
